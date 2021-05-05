cb9eb37506d3047202fdb14d451eaf79081323bb
Samir Landou <samirlandou@gmail.com>
2021-05-04 23:19:23 -0300

    Nivel 1 - React - Conceito de Propriedades

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import Header from './components/Header'
 
+
+/**
+ * 
+ * Conceito do React
+ * 
+ * Components
+ * propriedade
+ * Estado
+ */
 function App(){
     return (
        /* <div>
@@ -9,10 +18,24 @@ function App(){
         </div>
         */
        
-        //conceito de fragmentação abaixo para não usar a div
+        /**
+         * 
+         * conceito de fragmentação abaixo para não usar a div
+         * foram definidos dentro do primeiro <Header/> o conceito de "children" 
+         * e para acessar esses Children, basta chamar com {children}
+         * 
+         *
+         */
         <>
-            <Header/>
-            <Header/>
+            <Header title= "Menu">
+                <ul>
+                    <li>Homepage1</li>
+                    <li>Homepage2</li>
+                    <li>HomePage3</li>
+                </ul>
+            </Header>
+
+            <Header title= "Project"/>
         </>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
-export default function Header(){
+//Aplicando o conceito de propriedade na função com o parâmetro "props"
+/*export default function Header(props){
     return(
         <header>
-            <h1>ReactJS</h1>
+            <h1>{props.title}</h1>
         </header>
     )
+}*/
+
+
+//Aplicando a função com desestruturação do parâmetro.
+/*export default function Header({ title }){
+    return(
+        <header>
+            <h1>{title}</h1>
+        </header>
+    );
+}*/
+
+
+///Aplicando a função com desestruturação do parâmetro 
+//e com conceito com children(qu é todo o conteúdo que o componente recebeu)
+export default function Header({ title, children }){
+    return(
+        <header>
+            <h1>{title}</h1>
+            {children}
+        </header>
+    );
 }
