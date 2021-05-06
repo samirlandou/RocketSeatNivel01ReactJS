adf1c17d777ffa09435668fe6a63c6e472e3b614
Samir Landou <samirlandou@gmail.com>
2021-05-05 22:48:25 -0300

    Nivel 1 - React - Conceito de Estado e de Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Header from './components/Header'
+import React, {useState} from 'react';
+import Header from './components/Header';
 
 
 /**
@@ -8,9 +8,37 @@ import Header from './components/Header'
  * 
  * Components
  * propriedade
- * Estado
+ * Estado & Imutabilidade
+ * 
  */
 function App(){
+    //Essa linha pode ser desestruturado usando a sintaxe valida em baixo. 
+    //const projects = ['Desenvolvimento de app', 'Front-End web'];
+
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-End web']);
+
+
+
+    /**
+     * useState retorna um array com 2 posições
+     * 
+     * Na 1a Posição, ele retorno:
+     * 1. um Variante com seu valor inicial
+     * 2. Função para atualizarmos esse valor
+     * 
+     */
+
+    function handleAddProject(){
+        //projects.push(`Novo Project ${Date.now()}`);
+
+        //Como a array 'projects' é mutável, a gente aplica aqui o conceito de imutabilidade
+        //criando um novo array e copiando com '[...projects]' (isso se chama Spread operator) 
+        //tudo que já tem dentro do Projects (esse project é o project do constem). 
+        setProjects([...projects, `Novo Project ${Date.now()}`]);
+        
+        console.log(projects);
+    }
+
     return (
        /* <div>
             <Header/>
@@ -35,7 +63,14 @@ function App(){
                 </ul>
             </Header>
 
-            <Header title= "Project"/>
+            <Header title= "Project1"/>
+
+            <Header title= "Project2"/>
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar Project</button>
         </>
     );
 }
